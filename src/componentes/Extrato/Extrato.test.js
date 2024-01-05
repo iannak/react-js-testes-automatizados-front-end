53d54b7434a37baf03efbf152c50ce5c303461a9
Anna Karolina Franz <franz.karols@gmail.com>
2024-01-04 22:30:26 -0300

    test: Extrato

---
+++ src/componentes/Extrato/Extrato.test.js
@@ -0,0 +1,13 @@
+import { screen, render } from '@testing-library/react';
+import Extrato from './index';
+
+test('Deve renderizar uma lista de transações', () => {
+    const transacoes = [{
+        transacao: 'Depósito',
+        valor: 100,
+    }];
+
+    render(<Extrato transacoes={transacoes} />);
+    const lista = screen.getByRole('listitem');
+    expect(lista).toBeInTheDocument();
+})
