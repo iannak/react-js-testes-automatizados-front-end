38fbf900c04e42d3b6e69128e86e601590bc4edb
Anna Karolina Franz <franz.karols@gmail.com>
2024-01-04 21:12:19 -0300

    test: Menu

---
+++ src/componentes/Menu/Menu.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import Menu from './index';
+
+test('Deve renderizar um link para a pagina inicial', () => {
+  render(<Menu />);
+  const linkPaginaInicial = screen.getByText('Inicial');
+  expect(linkPaginaInicial).toBeInTheDocument();
+});
+
+test('Deve renderizar uma lista de link', () => {
+  render(<Menu />);
+  const listaDeLinks = screen.getAllByRole('link');
+  expect(listaDeLinks).toHaveLength(4);
+});
+
+test('Não deve renderizar o link para Extrato', () => {
+  render(<Menu />);
+  // verifica se tem elemento ou não
+  const linkExtrato = screen.queryByText('Extrato');
+  expect(linkExtrato).not.toBeInTheDocument();
+});
